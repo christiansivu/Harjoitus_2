367955a40ccb2e35e5d5b590ce9699527ef8a4f2
Christian Wiksten <christian.wiksten@ictpro.labra>
2020-11-18 12:25:06 +0200

    varmistettu kytkennän toimivuus

--- Harjoitus_2.ino
+++ Harjoitus_2.ino
@@ -1,19 +1,19 @@
-/*
- * Ohjelma näyttää miten saadaan Arduino kertomaan meille asioita. 
- * Sarjaporttiin kirjoittamista voi käyttää esimerkiksi vianhaku-
- * tilanteissa.
- */
-void setup() {
-  //Sarjaliikenteen alustus. 9600 edustaa standardoitua nopeutta
-  Serial.begin(9600);
-}
+#define LED1 5
+#define LEDON LOW
+#define LEDOFF HIGH
 
-int viive = 1000;
+void setup() {
+  
+pinMode (LED1,OUTPUT);
+Serial.begin(9600);
+}
+int viive = 500;
 void loop() {
-  /*
-   * Serial.println() funktio tulostaa parametrikenttään 
-   * sijoitetun tekstin, sekä rivinvaihdon.
-   */
-  Serial.println("moi!");
+  digitalWrite (LED1,LEDON);
+  Serial.println("ledi päällä");
+  delay(viive);
+
+  digitalWrite (LED1,LEDOFF);
+  Serial.println("ledi poispäältä");
   delay(viive);
 }
